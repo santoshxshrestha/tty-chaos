6b5b387ed3f911afefe8ca5f9f59a532236ec52f
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-18 13:52:37 +0545

    we have to also disable the raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use crossterm::terminal;
+#![allow(unused)]
+use crossterm::style::Print;
+use crossterm::{execute, terminal};
+use std::io::stdout;
 use std::thread::sleep;
 use std::{io, time};
 
@@ -6,7 +9,11 @@ fn main() -> io::Result<()> {
     //blocks certain features of the terminal
     // https://docs.rs/crossterm/latest/crossterm/terminal/index.html#raw-mode
     terminal::enable_raw_mode()?;
+    execute!(stdout(), Print("wtf".to_string()))?;
     sleep(time::Duration::from_secs(5));
+    print!("lol eof");
 
+    //if this is not done then the terminal will malfunction and remains same untill reopened
+    terminal::disable_raw_mode()?;
     Ok(())
 }
